e3599c24798952327cbd929e1fbbe449192fcfa7
DESKTOP-8TLV9IN\iii_student <blackcat5523@gmail.com>
2022-12-11 16:25:08 +0800

    修改

--- crawlar/crawler3.js
+++ crawlar/crawler3.js
@@ -1,20 +1,34 @@
 // axios await 版本
 // 把 query string 抽出來當變數，用 params 的方式去設定
 
-// 1. 安裝 npm i axios
-// 2. 引用 require
-// 3. 去讀官方文件
 const axios = require('axios');
-// http://54.71.133.152:3000/stocks?stockNo=2618&date=202211
+const fs = require('fs/promises');
+
+let p = new Promise((resolve, reject) => {
+    // error-first callback
+    fs.readFile('readFile.txt', 'utf-8', (err, data) => {
+        if (err) {
+            // 如果 err 有值，表示有錯誤發生
+            // 這裡應該要處理錯誤
+            reject(err);
+            // console.error('發生錯誤了', err);
+        } else {
+            // 進來這裡，表示 err 是空的 (可能是 null)
+            resolve(data);
+            // console.log('成功讀到資料:', data);
+        }
+    });
+});
 
 (async () => {
   try {
-    let stockNo = '2618';
-    let date = '20221111';
+    let dataFile = await p;
+    console.log(dataFile);
     let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
       params: {
-        stockNo,
-        date,
+        stockNo:dataFile,
+        date:'202211',
+        // date,
       },
     });
 
