aef0d9a3e89541ee09df18e3b25ce931e893e690
DESKTOP-8TLV9IN\iii_student <blackcat5523@gmail.com>
2022-12-24 10:29:41 +0800

    設定express處理靜態檔案

--- stock-be/sever.js
+++ stock-be/sever.js
@@ -18,15 +18,24 @@ app.get('/',(req,res,next)=>{
     console.log('這是首夜夜夜',req.mfeel31,req.dt);
     res.send('Hellow Express 9');
 });
+
+// localhost:3001/
+// app.use(express.static('./static'));
+// localhost:3001/2048/
+
 app.use((req, res, next) => {
     console.log('這裡是的一個中間件 C');
     next();
   });
+  
+  // 設定 express 處理靜態檔案
+// -> express 內建 -> 不需要安裝任何東西
+app.use('/2048', express.static('./2048'));
 
 app.get('/test',(req,res,next)=>{
     console.log('這是test頁面',req.dt);
     res.send('Hellow Test');
-});
+}); 
 app.use((req,res,next)=>{
     console.log('這是404頁面');
     res.send('沒有頁面');
