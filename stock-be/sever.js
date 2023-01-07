2d5e0efb1ccdb1e1f9f7b4fdf0ba3d28f28e5f0f
Gray <blackcat5523@gmail.com>
2023-01-07 11:38:04 +0800

    貼上

--- stock-be/sever.js
+++ stock-be/sever.js
@@ -66,11 +66,38 @@ app.get("/api/stocks", async (req, res, next) => {
 });
 app.get('/api/stock/:stockId',async(req,res,next)=>{
     console.log(req.params.stockId);
-    let [data] = await pool.query("SELECT * FROM stocks_prices WHERE stock_id=?",req.params.stockId);
+    //從前端拿到目前是要第幾頁,預設要是在第一頁,用或的邏輯就可以達到預設值
+    const page =req.query.page || 1;
+    // 總筆數？
+    let [results]=await pool.execute('SELECT COUNT(*) AS total FROM stock_prices WHERE stock_id=?',[req.params.stockId]);
+    const total =results[0].total;
+    // 共幾頁
+    const perPage=5;
+    const totalPage=Math.ceil(total / perPage);
+    //  計算 offset, limit (一頁有幾筆)
+    const limit =perPage;
+    const offset =perPage*(page-1);
+    // TODO: 根據 offset, limit 去取得資料
+    let [data] =await pool.execute('SELECT * FROM stock_prices WHERE stock_id=? ORDER BY date ')
+    // TODO: 把資料回覆給前端
+    res.json({
+        pagination:{
+            perPage,
+            totalPages,
+            page,
+        },
+        data
+    })
+    // 會用 prepared statement 的方式來避免發生 sql injection
+    // pool.query vs pool.execute
+    // let [data] = await pool.execute('SELECT * FROM stock_prices WHERE stock_id=?', [req.params.stockId]);
+    // res.json(data);
+
+    let [data] = await pool.query("SELECT * FROM stocks_prices WHERE stock_id=?",[req.params.stockId]);
     console.log("這是api/stocks");
     res.json(data);
 })
-// TODO:完成insert
+// 完成insert
 
 app.get("/test", (req, res, next) => {
     console.log("這是test頁面", req.dt);
